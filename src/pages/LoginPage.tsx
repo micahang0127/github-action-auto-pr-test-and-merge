26fd9dff61809939f55c58dc1abb11dae6646908
sanga.park <psa5013@hanmail.net>
2026-03-16 09:16:51 +0900

    feature/** -> dev -> main test #12

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -21,7 +21,7 @@ export function LoginPage() {
       // if (res.statusCode === 200) {
       localStorage.setItem('accessToken', res.data.accessToken)
       setLoggedIn(true)
-      navigated({ to: '/main' })
+      navigate({ to: '/main' })
       // }
     },
   })
