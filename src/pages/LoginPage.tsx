9d968e54e48a19e94fbba2acdbbd30a89f7178df
sanga.park <psa5013@hanmail.net>
2026-03-16 13:48:38 +0900

    dev 에러 테스트

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -21,7 +21,7 @@ export function LoginPage() {
       // if (res.statusCode === 200) {
       localStorage.setItem('accessToken', res.data.accessToken)
       setLoggedIn(true)
-      navigate({ to: '/main' })
+      navigate(ㅇ{ to: '/main' })
       // }
     },
   })
